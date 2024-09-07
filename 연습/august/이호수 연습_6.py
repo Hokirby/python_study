d53ae6b0d5af61b2789dc4e45dcaf5399b0c64a9
HoKirby <lakevely27@gmail.com>
2024-09-07 16:35:03 +0900

    practice

---
+++ 연습/august/이호수 연습_6.py
@@ -0,0 +1,78 @@
+# from random import randrange
+# a=(randrange(0,101,1))
+# for i in range(3):
+#     print("체력 : {}".format(3-i))
+#     b=int(input("수입력: "))
+#     if a>b:
+#         print("UP")
+#     elif a<b:
+#         print("DOWN")
+#     else:
+#         print("GOOD")
+
+# from random import randrange
+# a=(randrange(0,101,1))
+# i=1
+# while True:
+#     b=int(input("수입력: "))
+#     i+=1
+#     if a>b:
+#         print("UP")
+#     elif a<b:
+#         print("DOWN")
+#     else:
+#         print("GOOD")
+#         print("{}번만에 맞춤".format(i))
+#         break
+
+print("난이도 설정")
+print("1:easy [1~10]")
+print("2:normal [1~100]")
+print("3:hard [1~1000]")
+
+a=int(input("선택:  "))
+if a==1:
+    from random import randrange
+    b=(randrange(0,11,1))
+    i=1
+    while True:
+        c=int(input("수입력: "))
+        i+=1
+        if b>c:
+            print("UP")
+        elif b<c:
+            print("DOWN")
+        else:
+            print("GOOD")
+            print("{}번만에 맞춤".format(i))
+            break
+if a==2:
+    from random import randrange
+    b=(randrange(0,101,1))
+    i=1
+    while True:
+        c=int(input("수입력: "))
+        i+=1
+        if b>c:
+            print("UP")
+        elif b<c:
+            print("DOWN")
+        else:
+            print("GOOD")
+            print("{}번만에 맞춤".format(i))
+            break
+if a==3:
+    from random import randrange
+    b=(randrange(0,1001,1))
+    i=1
+    while True:
+        c=int(input("수입력: "))
+        i+=1
+        if b>c:
+            print("UP")
+        elif b<c:
+            print("DOWN")
+        else:
+            print("GOOD")
+            print("{}번만에 맞춤".format(i))
+            break
---
+++ 연습/july/이호수 연습_16.py
@@ -0,0 +1,32 @@
+# st="It is a fun python class"
+# cnt_a=cnt_s=cnt=0
+# for i in st:
+#     cnt+=1
+#     if i=='a':
+#         cnt_a+=1
+#     elif i=='s':
+#         cnt_s+=1
+# print("문자열의 길이 {}".format(cnt))
+# print("문자 'a'의 개수: {}".format(cnt_a))
+# print("문자 's'의 개수 : {}".format(cnt_s))
+
+# all=st.count("")
+# a=st.count("a")
+# b=st.count("s")
+# print("문자열의 길이 : {}".format(all-1))
+# print("문자 'a'의 개수 : {}".format(a))
+# print("문자 's'의 개수 : {}".format(b))
+
+# st='Have a nice day'
+# a=st.count('a')
+# b=st.count('day')
+# c=st.count('dak')
+# print("'a'문자열 개수 : {}".format(a))
+# print("'day'문자열 개수: {}".format(b))
+# print("'dak'문자열 개수 : {}".format(c))
+
+# st= """김개똥 -2017년 03월 24일
+# 홍길동구리 -2015년 04월 02일
+# 선우선녀 -2018년 05월14일"""
+# st=st.replace("-",":").replace("2015","1999").replace("2017","1999").replace("2018","1999")
+# print("st= {} ".format(st))
