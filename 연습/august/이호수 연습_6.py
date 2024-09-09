e125ae680c82941753e369c8c3a846e83d01a718
HoKirby <lakevely27@gmail.com>
2024-09-09 22:49:38 +0900

    practice

--- 연습/august/이호수 연습_6.py
+++ 연습/august/이호수 연습_6.py
@@ -25,54 +25,57 @@
 #         print("{}번만에 맞춤".format(i))
 #         break
 
-print("난이도 설정")
-print("1:easy [1~10]")
-print("2:normal [1~100]")
-print("3:hard [1~1000]")
+# print("난이도 설정")
+# print("1:easy [1~10]")
+# print("2:normal [1~100]")
+# print("3:hard [1~1000]")
 
-a=int(input("선택:  "))
-if a==1:
-    from random import randrange
-    b=(randrange(0,11,1))
-    i=1
-    while True:
-        c=int(input("수입력: "))
-        i+=1
-        if b>c:
-            print("UP")
-        elif b<c:
-            print("DOWN")
-        else:
-            print("GOOD")
-            print("{}번만에 맞춤".format(i))
-            break
-if a==2:
-    from random import randrange
-    b=(randrange(0,101,1))
-    i=1
-    while True:
-        c=int(input("수입력: "))
-        i+=1
-        if b>c:
-            print("UP")
-        elif b<c:
-            print("DOWN")
-        else:
-            print("GOOD")
-            print("{}번만에 맞춤".format(i))
-            break
-if a==3:
-    from random import randrange
-    b=(randrange(0,1001,1))
-    i=1
-    while True:
-        c=int(input("수입력: "))
-        i+=1
-        if b>c:
-            print("UP")
-        elif b<c:
-            print("DOWN")
-        else:
-            print("GOOD")
-            print("{}번만에 맞춤".format(i))
-            break
+# a=int(input("선택:  "))
+# if a==1:
+#     from random import randrange
+#     b=(randrange(0,11,1))
+#     i=1
+#     while True:
+#         c=int(input("수입력: "))
+#         i+=1
+#         if b>c:
+#             print("UP")
+#         elif b<c:
+#             print("DOWN")
+#         else:
+#             print("GOOD")
+#             print("{}번만에 맞춤".format(i))
+#             break
+# if a==2:
+#     from random import randrange
+#     b=(randrange(0,101,1))
+#     i=1
+#     while True:
+#         c=int(input("수입력: "))
+#         i+=1
+#         if b>c:
+#             print("UP")
+#         elif b<c:
+#             print("DOWN")
+#         else:
+#             print("GOOD")
+#             print("{}번만에 맞춤".format(i))
+#             break
+# if a==3:
+#     from random import randrange
+#     b=(randrange(0,1001,1))
+#     i=1
+#     while True:
+#         c=int(input("수입력: "))
+#         i+=1
+#         if b>c:
+#             print("UP")
+#         elif b<c:
+#             print("DOWN")
+#         else:
+#             print("GOOD")
+#             print("{}번만에 맞춤".format(i))
+#             break
+
+# a=[i for i in range(1,11)]
+# print(a)
