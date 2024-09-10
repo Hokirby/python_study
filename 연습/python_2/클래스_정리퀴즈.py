e4f94dff8bd11b3023056c81d6e7ef1a4090122a
HoKirby <lakevely27@gmail.com>
2024-09-10 21:47:50 +0900

    practice

---
+++ 연습/python_2/클래스_정리퀴즈.py
@@ -0,0 +1,77 @@
+# # Quiz1
+
+# class Test1:
+#     def __init__(self):
+#         self.hi="펭하~"
+#         print(self.hi)
+
+# # Quiz2
+
+# class Test2:
+#     def __init__(self):
+#         self.name='펭수'
+#         self.email="abc@naver.com"
+
+# peng=Test2()
+# print(peng.name)
+# print(peng.email)
+
+# # Quiz3
+
+# class Test3:
+#     def __init__(self):
+#         self.name='펭수'
+#         self.email="abc@naver.com"
+    
+#     def a(self):
+#         print(self.name)
+#         print(self.email)
+
+# # Quiz4
+
+# class Test4:
+#     def __init__(self,name,phone,address):
+#         self.name=name
+#         self.phone=phone
+#         self.address=address
+
+# peng=Test4("펭수","010-1234-5678","남극")
+# print(peng.name)
+# print(peng.phone)
+# print(peng.address)
+
+# # Quiz5
+
+# class Test5:
+#     def __init__(self,dict):
+#         self.dict=dict
+
+# info={"name":"펭수",
+#       'address':"남극",
+#       'phone':"010-1234-5678"
+#         }
+
+# peng=Test5(info)
+# print(peng.dict)
+
+# # Quiz6
+
+# class Test6:
+#     def __init__(self):
+#         pass
+
+#     def calc1(self,num):
+#         self.plus+=num
+#         self.minus-=num
+
+# # Quiz7
+
+# class Test7:
+#     def __init__(self):
+#         pass
+
+#     def calc2(self,num):
+#         self.multiple*=num
+#         self.divide/=num
+
+# Test7=Test6()
