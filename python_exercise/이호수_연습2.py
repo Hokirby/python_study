3ab99a1d94f63c13bf1e0f20246830f61d925ea7
HoKirby <lakevely27@gmail.com>
2024-09-14 15:20:37 +0900

    exercise

---
+++ python_exercise/이호수_연습2.py
@@ -0,0 +1,67 @@
+# # 011
+
+# 삼성전자 = 50000
+# 총평가금액 = 삼성전자 * 10
+# print(총평가금액)
+
+# # 012
+
+# 시가총액 = 2980000000000
+# 현재가 = 50000
+# PER = 15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+
+# # 013
+
+# s = "hello" # 바인딩
+# t = "python"
+# print(s,t)
+# print(s,"!",t) # 중간에 띄어쓰기
+# print(s+"!",t) # +기호를 사용하면 바로 뒤에 출력됨
+
+# # 014
+
+# 2 + 2 * 3
+# 8
+
+# # 015
+
+# a = 128
+# print(type(a)) # <class 'int'>
+# a = '132'
+# print(type(a)) # <class 'str'>
+
+# # 016
+
+# num_str = '720'
+# num_int = int(num_str) # 형변환
+# print(num_int, type(num_int))
+
+# # 017
+
+# num = 100
+# num_str = str(num)
+# print(num_str, type(num_str))
+
+# # 018
+
+# data = "15.79"
+# data = float(data)
+# print(data, type(data))
+
+# # 019
+
+# year = '2020'
+# year = int(year)
+# print(year+2)
+# print(year+3)
+# print(year+4)\
+
+# # 020
+
+# installment = 48584
+# months = 36
+# price = installment * months
+# print(price)
---
+++ python_exercise/이호수_연습3.py
@@ -0,0 +1,14 @@
+# # 021
+
+# letters = 'python'
+# print(letters[0],letters[2])
+
+# # 022
+
+# license_plate = "175허 9795"
+# print(license_plate[5:9]) # 슬라이싱
+
+# # 023
+
+# string = "홀짝홀짝홀짝"
+# string.
