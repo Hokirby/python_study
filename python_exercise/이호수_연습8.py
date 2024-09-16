80a8f57116029fecbde5aa266a6835724d953f28
HoKirby <lakevely27@gmail.com>
2024-09-16 21:32:43 +0900

    exercise

---
+++ python_exercise/이호수_연습8.py
@@ -0,0 +1,65 @@
+# # 071
+
+# my_variable = ()
+# print(type(my_variable))
+
+# # 072
+
+# my_variable = ("닥터 스트레인지","스플릿","럭키")
+
+# # 073
+
+# my_tuple = (1,)
+
+# # 074
+
+# t = (1,2,3)
+# t[0] = 'a' # TypeError tuple은 원소의 값 변경할 수 없음
+
+# # 075
+
+# t = 1, 2, 3, 4
+# print(type(t))
+
+# a, b, c, d = 1, 2, 3, 4
+# print(a)
+
+# # 076
+
+# t = ('a','b','c')
+# t = ('A','b','c')
+# print(t)
+
+# # 077
+
+# interest = ('삼성전자','LG전자','SK Hynix')
+
+# a = []
+# for i in interest:
+#     a.append(i)
+# print(a)
+
+# data = list(interest) # 리스트 만들기(내용)
+# print(data)
+
+# # 078
+
+# interest = ['삼성전자', 'LG전자', 'SK Hynix']
+
+# interest = tuple(interest) # 튜플 만들기(내용)
+# # 튜플에는 문자열을 추가할 수 없다
+# print(interest)
+
+# # 079
+
+# temp = ('apple', 'banana', 'cake')
+# a, b, c = temp
+# print(a, b, c)
+
+# # 080
+
+# a = tuple(range(2,100,2))
+# print(a)
+
+# b= range(1,101,2)
+# print(b)
