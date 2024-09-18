a67b51e9c563afc934a0154edbc4cd34b98344e9
HoKirby <lakevely27@gmail.com>
2024-09-19 00:24:38 +0900

    exercise

--- python_exercise/이호수_연습_011.py
+++ python_exercise/이호수_연습_011.py
@@ -1,6 +1,6 @@
 # # 101
 
-# 'bool' # True or False 의 데이터 타입
+# 'bool' # boolean: True or False 의 데이터 타입
 
 # # 102
 
@@ -17,4 +17,41 @@
 
 # # 105
 
-# print ((3 == 3) and (4 != 3)) # True
+# print ((3 == 3) and (4 != 3)) # True/ != : 같지 않다. ==의 반대
+
+# # 106
+
+# print(3 => 4) # SyntaxError / 없는 연산자 사용  >= 로 바꿔야함
+
+# # 107
+
+# if 4 < 3:
+#     print("Hello, World")
+
+# # 108
+
+# if 4 < 3:
+#     print("Hello World.")
+# else:
+#     print("Hi, there.")
+
+# # 109
+
+# if True : # True : 조건을 충족함 / 실행 
+#     print ("1")
+#     print ("2")
+# else :
+#     print("3")
+# print("4")
+
+# # 110
+
+# if True :
+#     if False:
+#         print("1")
+#         print("2")
+#     else:
+#         print("3")
+# else :
+#     print("4")
+# print("5")
---
+++ python_exercise/이호수_연습_012.py
@@ -0,0 +1,84 @@
+# # 111
+
+# a = input()
+# print(a*2)
+
+# # 112
+
+# num = int(input("숫자를 입력하세요 : "))
+# print(num+10)
+
+# # 113
+
+# num = int(input("수 입력 : "))
+# if num%2==0:
+#     print("짝수")
+# else:
+#     print("홀수")
+
+# # 114
+
+# userInput = int(input("입력값 : "))
+# result = userInput + 20
+# if result > 255:
+#     print(입력값 : 255)
+# else:
+#     print("출력값 : {}".format(result))
+
+# # 115
+
+# userInput = int(input("입력값 : "))
+# result = userInput - 20
+# if result < 0:
+#     print("출력값 : 0")
+# elif result > 255:
+#     print("출력값 : 255")
+# else:
+#     print(result)
+
+# # 116
+
+# timeInput = input("현재시간 : ")
+# min = timeInput[-2:]
+# if min == "00":
+#     print("정각입니다.")
+# else:
+#     print("정각이 아닙니다")
+
+# # 117
+
+# fruit = ["사과", "포도", "홍시"]
+# favInput = input("좋아하는 과일은? ")
+# if favInput in fruit:
+#     print("정답입니다.")
+# else:
+#     print("오답입니다.")
+
+# # 118
+
+# warn_investment_list = ["Microsoft", "Google", "Naver", "Kakao", "SAMSUNG", "LG"]
+# frontier = input("투자 종목명 : ")
+# if frontier in warn_investment_list:
+#     print("투자 경고 종목입니다.")
+# else:
+#     print("투자 경고 종목이 아닙니다.")
+
+# # 119
+
+# fruit = {"봄" : "딸기", "여름": "토마토", "가을": "사과"}
+# keyInput = input("제가 좋아하는 계절은 : ")
+# a = fruit.keys()
+# if keyInput in a:
+#     print("정답입니다.")
+# else:
+#     print("오답입니다")
+
+# # 120
+
+# fruit = {"봄" : "딸기", "여름": "토마토", "가을": "사과"}
+# valueInput = input("좋아하는 과일은? ")
+# b = fruit.values()
+# if valueInput in b:
+#     print("정답입니다.")
+# else:
+#     print("오답입니다.")
