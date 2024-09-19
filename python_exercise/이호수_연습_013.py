0f4c1221f9771578467031cc75101c4379eb2260
HoKirby <lakevely27@gmail.com>
2024-09-19 15:24:37 +0900

    exrcise

---
+++ python_exercise/이호수_연습_013.py
@@ -0,0 +1,29 @@
+# # 121
+
+# userInput = input("영어 문자 한 개 입력 : ")
+
+# if userInput.islower() == True:
+#     print(userInput.upper())
+# else:
+#     print(userInput.lower())
+
+# # 122
+
+# userScore = int(input("score : "))
+
+# if 100 >= userScore >= 81:
+#     print("grade is A")
+# elif 80 >= userScore >= 61:
+#     print("grade is B")
+# elif 60 >= userScore >= 41:
+#     print("grade is C")
+# elif 40 >= userScore >= 21:
+#     print("grade is D")
+# elif 20 >= userScore >= 0:
+#     print("grade is E")
+# else:
+#     print("잘못입력하셨습니다.")
+
+# # 123
+
+userInput = int(input("입력 : ")) 
